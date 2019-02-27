8ab02315ed5b80d43bd71c2323b6b5d2c6cdb387
WebMed <a.aisati@gmail.com>
2019-02-27 22:50:57 +0100

    Clean up

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using IdentityModel.Client;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
@@ -13,15 +12,6 @@ namespace Client
     {
         private static async Task Main()
         {
-            // discover endpoints from metadata
-            var client = new HttpClient();
-
-            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
-            if (disco.IsError)
-            {
-                Console.WriteLine(disco.Error);
-                return;
-            }
 
 
             // call api
--- src/ResourceOwnerClient/Program.cs
+++ src/ResourceOwnerClient/Program.cs
@@ -13,51 +13,8 @@ namespace ResourceOwnerClient
     {
         private static async Task Main()
         {
-            // discover endpoints from metadata
-            var client = new HttpClient();
 
-            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
-            if (disco.IsError)
-            {
-                Console.WriteLine(disco.Error);
-                return;
-            }
 
-            // request token
-            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
-            {
-                Address = disco.TokenEndpoint,
-                ClientId = "ro.client",
-                ClientSecret = "secret",
-
-                UserName = "alice",
-                Password = "password",
-                Scope = "api1"
-            });
-
-            if (tokenResponse.IsError)
-            {
-                Console.WriteLine(tokenResponse.Error);
-                return;
-            }
-
-            Console.WriteLine(tokenResponse.Json);
-            Console.WriteLine("\n\n");
-
-            // call api
-            var apiClient = new HttpClient();
-            apiClient.SetBearerToken(tokenResponse.AccessToken);
-
-            var response = await apiClient.GetAsync("http://localhost:5001/identity");
-            if (!response.IsSuccessStatusCode)
-            {
-                Console.WriteLine(response.StatusCode);
-            }
-            else
-            {
-                var content = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(JArray.Parse(content));
-            }
         }
     }
 }
